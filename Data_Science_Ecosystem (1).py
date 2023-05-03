2bb17e0a54a93fadfb8a732f8f0eea0436002a51
Patrick Julius Kuria <132275802+Patrick-Julius-Kuria@users.noreply.github.com>
2023-05-03 14:25:23 +0300

    Add files via upload

---
+++ Data_Science_Ecosystem (1).py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# #### Objectives
+# 
+# Some of the key take aways from this course are:
+# 
+#  -List the languages used by data scientists.
+#  
+#  -List the commonly used libraries that data scientists use.
+#  
+#  -Create a table using jupiter notebook.
+#  
+#  -Perform mathematical computations and expressions.
+#  
+# 
+
+# Some of the popular languages that Data Scientists use are:
+# 
+#     1.Python
+#     2.Java
+#     3.C++
+#     4.R
+#     5.Javascript
+#     6.SQL
+#     7.Apache MLlib
+#     8.Weka
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 
+#    1.RStudio
+#    
+#    2.Jupiter Notebook
+#    
+#    3.IBM Watson Studio
+#    
+#    4.Anaconda
+#    
+#    5.Oracle cloud infrastructure
+#    
+#    6.Glueviz
+#    
+#    7.Datalore
+#    
+#    8.ggplot2
+#    
+#    9.Tensorflow
+
+# |Data Science Tools|
+# |-------|
+# |Rstudio|
+# |Jupiter Notebook|
+# |IBM Watson Studio|
+# |Github|
+# 
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[20]:
+
+
+#This a simple arithmetic expression to mutiply then add integers
+
+(3*4)+5
+
+
+# In[21]:
+
+
+#This will convert 200 minutes to hours by diving by 60
+# Total Number of minutes TM = 200
+# Minutes per Hour, mph= 60
+# Converting minutes to hours , minutes_to_hours
+
+(200/60)
+
+
+
+
+
+
+
+
+# ## Author
+# 
+# Patrick Julius Kuria
+
+# In[ ]:
+
+
+
+
